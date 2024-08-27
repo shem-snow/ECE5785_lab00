6e3ac06dbf35e831367954987d1f14b01e1b1963
Shem Snow <shem-snow@hotmail.com>
2024-08-26 19:16:30 -0600

    Built the blink project and flashed it onto the pico

--- blink/build/pico-sdk/cmake_install.cmake
+++ blink/build/pico-sdk/cmake_install.cmake
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -39,16 +39,16 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/shem/5785/lab00/blink/build/pico-sdk/tools/cmake_install.cmake")
+  include("/home/shem/5785/blink/build/pico-sdk/tools/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/shem/5785/lab00/blink/build/pico-sdk/src/cmake_install.cmake")
+  include("/home/shem/5785/blink/build/pico-sdk/src/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/shem/5785/lab00/blink/build/pico-sdk/docs/cmake_install.cmake")
+  include("/home/shem/5785/blink/build/pico-sdk/docs/cmake_install.cmake")
 endif()
 
